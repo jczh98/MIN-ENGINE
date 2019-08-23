aec73f60b4b087ca89ec68bd136fbbf310f60c4c
neverfelly <neverfelly@gmail.com>
2019-08-23 19:29:57 +0800

    Finish first
    Signed-off-by: neverfelly <neverfelly@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -30,23 +30,56 @@ Vector3f color(const Ray &r, Hitable *world, int depth) {
   }
 }
 
+Hitable *RandomScene() {
+  int n = 500;
+  Hitable **list = new Hitable *[n + 1];
+  list[0] = new Sphere(Vector3f(0, -1000, 0), 1000, new Lambertian(Vector3f(0.5, 0.5, 0.5)));
+  int i = 1;
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      float choose_mat = drand48();
+      Vector3f center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+      if ((center - Vector3f(4, 0.2, 0)).Length() > 0.9) {
+        if (choose_mat < 0.8) {  // diffuse
+          list[i++] = new Sphere(
+              center, 0.2,
+              new Lambertian(Vector3f(drand48() * drand48(),
+                                      drand48() * drand48(),
+                                      drand48() * drand48()))
+          );
+        } else if (choose_mat < 0.95) { // metal
+          list[i++] = new Sphere(
+              center, 0.2,
+              new Metal(Vector3f(0.5 * (1 + drand48()),
+                                 0.5 * (1 + drand48()),
+                                 0.5 * (1 + drand48())),
+                        0.5 * drand48())
+          );
+        } else {  // glass
+          list[i++] = new Sphere(center, 0.2, new Dielectric(1.5));
+        }
+      }
+    }
+  }
+
+  list[i++] = new Sphere(Vector3f(0, 1, 0), 1.0, new Dielectric(1.5));
+  list[i++] = new Sphere(Vector3f(-4, 1, 0), 1.0, new Lambertian(Vector3f(0.4, 0.2, 0.1)));
+  list[i++] = new Sphere(Vector3f(4, 1, 0), 1.0, new Metal(Vector3f(0.7, 0.6, 0.5), 0.0));
+
+  return new HitableList(list, i);
+}
+
 int main(int argc, char *argv[]) {
-  int nx = 200;
-  int ny = 100;
-  int ns = 100;
+  int nx = 1200;
+  int ny = 800;
+  int ns = 10;
   chihaya::Image image(nx, ny, 3);
-  Hitable *list[5];
-  list[0] = new Sphere(Vector3f(0, 0, -1), 0.5, new Lambertian(Vector3f(0.1, 0.2, 0.5)));
-  list[1] = new Sphere(Vector3f(0, -100.5, -1), 100, new Lambertian(Vector3f(0.8, 0.8, 0.0)));
-  list[2] = new Sphere(Vector3f(1, 0, -1), 0.5, new Metal(Vector3f(0.8, 0.6, 0.2), 0.3));
-  list[3] = new Sphere(Vector3f(-1, 0, -1), 0.5, new Dielectric(1.5));
-  list[4] = new Sphere(Vector3f(-1, 0, -1), -0.45, new Dielectric(1.5));
-  Hitable *world = new HitableList(list, 5);
-  Vector3f lookfrom(3, 3, 2);
-  Vector3f lookat(0, 0, -1);
-  Float dist_to_focus = (lookfrom - lookat).Length();
-  Float aperture = 2.0;
-  Camera camera(lookfrom, lookat, Vector3f(0, 1, 0), 20, Float(nx)/Float(ny), aperture, dist_to_focus);
+  Hitable *world = RandomScene();
+  Vector3f lookfrom(13, 2, 3);
+  Vector3f lookat(0, 0, 0);
+  Float dist_to_focus = 10.0;
+  Float aperture = 0.1;
+  Camera camera(lookfrom, lookat, Vector3f(0, 1, 0), 20, Float(nx) / Float(ny), aperture, dist_to_focus);
   for (int j = ny - 1; j >= 0; --j) {
     for (int i = 0; i < nx; ++i) {
       Vector3f col(0, 0, 0);
@@ -57,6 +90,7 @@ int main(int argc, char *argv[]) {
         Vector3f p = r(2);
         col += color(r, world, 0);
       }
+      std::cout << i << " " << j << std::endl;
       col /= ns;
       col = Vector3f(sqrt(col.x), sqrt(col.y), sqrt(col.z));
       int ir = int(255.99 * col.x);
