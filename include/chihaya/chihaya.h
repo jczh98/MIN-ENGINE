a43c3d3cf44803a42e4bb43728dbf98a5460cfee
neverfelly <neverfelly@gmail.com>
2019-09-02 20:11:07 +0800

    Rewrite 2/n
    Signed-off-by: neverfelly <neverfelly@gmail.com>

--- include/chihaya/chihaya.h
+++ include/chihaya/chihaya.h
@@ -21,6 +21,82 @@
 #include <fmt/format.h>
 #include <glog/logging.h>
 
+//******************************************************************************
+//                                 System State
+//******************************************************************************
+
+// Reference:
+// https://blog.kowalczyk.info/article/j/guide-to-predefined-macros-in-c-compilers-gcc-clang-msvc-etc..html
+
+// Platforms
+
+// Windows
+#if defined(_WIN64)
+#define PLATFORM_WINDOWS
+#endif
+
+#if defined(_WIN32) && !defined(_WIN64)
+static_assert(false, "32-bit Windows systems are not supported")
+#endif
+
+// Linux
+#if defined(__linux__)
+#define PLATFORM_LINUX
+#endif
+
+// OSX
+#if defined(__APPLE__)
+#define PLATFORM_OSX
+#endif
+
+#if (defined(TC_PLATFORM_LINUX) || defined(TC_PLATFORM_OSX))
+#define PLATFORM_UNIX
+#endif
+
+// Compilers
+
+
+// MSVC
+#if defined(_MSC_VER)
+#define COMPILER_MSVC
+#endif
+
+// MINGW
+#if defined(__MINGW64__)
+#define COMPILER_MINGW
+#endif
+
+// gcc
+#if defined(__GNUC__)
+#define COMPILER__GCC
+#endif
+
+// clang
+#if defined(__clang__)
+#define COMPILER_CLANG
+#endif
+
+#if defined(COMPILER_MSVC)
+#define CHIHAYA_ALIGNED(x) __declspec(align(x))
+#else
+#define CHIHAYA_ALIGNED(x) __attribute__((aligned(x)))
+#endif
+
+#if __cplusplus >= 201703L
+#define CHIHAYA_CPP17
+#else
+#if defined(COMPILER_CLANG)
+static_assert(false, "For clang compilers, use -std=c++17");
+#endif
+static_assert(__cplusplus >= 201402L, "C++14 required.");
+#define CHIHAYA_CPP14
+#endif
+
+// Do not disable assert...
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
 #define CHIHAYA_NAMESPACE_BEGIN namespace chihaya {
 
 #define CHIHAYA_NAMESPACE_END }
