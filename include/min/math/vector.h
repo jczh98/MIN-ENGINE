40729d1f973f7f49a4f72681c3287aec34672bb2
neverfelly <neverfelly@gmail.com>
2019-09-14 14:13:57 +0800

    Refactor names
    Signed-off-by: neverfelly <neverfelly@gmail.com>

---
+++ include/min/math/vector.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2018-2019, neverfelly <neverfelly@gmail.com>
+ *
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ */
+#ifndef MIN_INCLUDE_MIN_MATH_VECTOR_H_
+#define MIN_INCLUDE_MIN_MATH_VECTOR_H_
+
+#include "vector_3d.h"
+
+MIN_NAMESPACE_BEGIN
+
+template <int dim, typename T>
+class VectorND;
+
+MIN_NAMESPACE_END
+
+#endif //MIN_INCLUDE_MIN_MATH_VECTOR_H_
