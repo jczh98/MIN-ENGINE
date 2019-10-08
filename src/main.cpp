c7a38c69c3d243b6b5697fdebc1a4667793cc09a
neverfelly <neverfelly@gmail.com>
2019-10-08 17:31:29 +0800

    Rewrite 9/n(Update pugixml and tbb)
    Signed-off-by: neverfelly <neverfelly@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-#include <min/math/vector.h>
+#include <pugixml.hpp>
 
 int main(int argc, char *argv[]) {
+  pugi::xml_document doc;
   return 0;
 }
