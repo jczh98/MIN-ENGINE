9920ff12dbed86f4559271bf1a0fad2e29442dbd
neverfelly <neverfelly@gmail.com>
2019-10-13 00:28:53 +0800

    Fix cmake files
    Signed-off-by: neverfelly <neverfelly@gmail.com>

---
+++ src/opengl/model.cc
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2018-2019, neverfelly <neverfelly@gmail.com>
+ *
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ */
+#include <min/opengl/model.h>
