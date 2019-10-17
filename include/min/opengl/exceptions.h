5fa937e8abcc149dd83fd55b612d2cf641d47179
neverfelly <neverfelly@gmail.com>
2019-10-17 11:48:17 +0800

    Add opengl wrappers
    Signed-off-by: neverfelly <neverfelly@gmail.com>

---
+++ include/min/opengl/exceptions.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2018-2019, neverfelly <neverfelly@gmail.com>
+ *
+ * This software is licensed under the terms of the MIT License.
+ * See COPYING for further information.
+ */
+#pragma once
+
+#include <min/common/util.h>
+
+MIN_NAMESPACE_BEGIN
+namespace gl {
+class OpenGLException : public std::runtime_error {
+
+};
+}
+MIN_NAMESPACE_END
