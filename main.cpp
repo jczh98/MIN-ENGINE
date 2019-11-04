2557a4ae96f9e1f9cff77e9f9b79648716a10f2f
neverfelly <neverfelly@gmail.com>
2019-11-04 15:12:23 +0800

    Add eigen3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include <Eigen/Geometry>
+
+using namespace Eigen;
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  Vector3f vec1(1, 2, 3);
+  Vector3f vec2(2, 3, 4);
+  std::cout << vec1 + vec2 << std::endl;
   return 0;
 }
