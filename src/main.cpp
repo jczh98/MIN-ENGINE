3c50f1e67f32a59280dfa34fe786196677ae902b
neverfelly <neverfelly@gmail.com>
2019-11-08 22:48:59 +0800

    Update dependency
    Signed-off-by: neverfelly <neverfelly@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "imgui/imgui_impl_opengl3.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <Eigen/Dense>
 #include <iostream>
+#include <nf/math/linalg.h>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
